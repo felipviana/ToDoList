4dd9d68145901fb3c0167260d6e01e24a93c659c
fabio <fabiofelipevianasantosfacul@gmail.com>
2024-10-31 01:47:27 -0300

    ToDo entity creation

---
+++ src/main/java/com/felps/ToDoList/entity/ToDo.java
@@ -0,0 +1,25 @@
+package com.felps.ToDoList.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name="todos")
+public class ToDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean done;
+    private int priority;
+
+}
